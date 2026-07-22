774ca5d54a5c8be4fff9fcd6044739e75d6830f7
Lautaro Amengual <amengual.ln@gmail.com>
2026-07-21 21:09:03 -0300

    New meetup sources for events

--- lib/meetup-groups.ts
+++ lib/meetup-groups.ts
@@ -1,7 +1,9 @@
 export const MEETUP_GROUPS = [
   { slug: "aws-security-usergroup-argentina", name: "AWS Security UserGroup Argentina" },
   { slug: "aws-women-in-cloud-buenos-aires-meetup", name: "AWS Women in Cloud Buenos Aires" },
-  { slug: "sysarmy", name: "Sysarmy" },
+  { slug: "platform-Engineers-buenos-aires", name: "Platform engineers Buenos Aires" },
+  { slug: "dataenginear", name: "dataengine.ar" },
+  { slug: "sysarmy", name: "Sysarmy" }
 ] as const;
 
 export type MeetupGroupSlug = (typeof MEETUP_GROUPS)[number]["slug"];
